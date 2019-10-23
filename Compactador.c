5dc7978509b2f5b9043644da1143edafba23b5d8
KaiFlame <42344215+KaiFlame@users.noreply.github.com>
2019-10-23 08:09:11 -0300

    criarArvore()

--- Compactador.c
+++ Compactador.c
@@ -11,8 +11,50 @@ typedef struct No
 
 } No;
 
+void incluir(No *vetor, int *size, No newNo)
+{
+    int i;
+    for(i = 0; i < *size; i++)
+        if(newNo.frequencia < vetor[i].frequencia)
+            break;
+    for(int e = *size; e > i; e--)
+        vetor[e] = vetor[e-1];
+    vetor[i] = newNo;
+    (*size)++;
+}
 
-void main()
+No retirar(No *vetor, int *size)
+{
+   No retorno = vetor[0];
+   (*size)--;
+   for(int i = 0; i < *size; i++)
+        vetor[i] = vetor[i+1];
+   return retorno;
+
+}
+
+No CriarArvore(No *vetor, int *size)
+{
+    while(*size > 1)
+    {
+        No newNo;
+        No esquerda = retirar(vetor, size);
+        No direita = retirar(vetor, size);
+
+        newNo.esquerda = &esquerda;
+        newNo.direita = &direita;
+
+        newNo.caracter = '\0';
+        newNo.frequencia = esquerda.frequencia + direita.frequencia;
+
+        incluir(vetor, size, newNo);
+    }
+
+    return vetor[0];
+
+}
+
+int main()
 {
 
     /*int arroz = 1;
@@ -20,14 +62,14 @@ void main()
     *batata = 0;
     printf("%d", arroz);*/
 
-    char teste[7];
-    strcpy(teste, "batata");
+    char teste[20];
+    strcpy(teste, "paralelepipedo");
 
-    No fila[7];
+    No fila[20];
     int tamanho = 0;
     No *anterior;
 
-    for(int i = 0; i < 7; i++)
+    for(int i = 0; i < 14; i++)
     {
         if(tamanho != 0)
         {
@@ -37,8 +79,18 @@ void main()
                 if(fila[indice].caracter == teste[i])
                 {
                     fila[indice].frequencia++;
-                    int existe = 1;
+                    while (indice < tamanho-1 && fila[indice].frequencia > fila[indice+1].frequencia)
+                    {
+                        No aux = fila[indice];
+                        fila[indice] = fila[indice+1];
+                        fila[indice+1] = aux;
+                        indice++;
+                    }
+                    existe = 1;
+                    break;
+
                 }
+
             }
             if(existe == 0)
             {
@@ -46,8 +98,7 @@ void main()
                 novoNo.caracter = teste[i];
                 novoNo.frequencia = 1;
                 anterior = &novoNo;
-                tamanho++;
-                fila[i] = novoNo;
+                incluir(fila, &tamanho, novoNo);
             }
 
         }
@@ -58,18 +109,23 @@ void main()
             novoNo.caracter = teste[i];
             novoNo.frequencia = 1;
             anterior = &novoNo;
-            tamanho++;
-            fila[i] = novoNo;
+            incluir(fila, &tamanho, novoNo);
 
         }
 
 
     }
 
-    printf("%c, %c, %c, %d, %d, %d", fila[0].caracter, fila[1].caracter, fila[2].caracter, fila[0].frequencia, fila[1].frequencia, fila[2].frequencia);
+ for(int i = 0; i < tamanho; i ++)
+            printf("%c, %d ", fila[i].caracter, fila[i].frequencia);
+        printf("\n");
+
+        printf("%d", CriarArvore(fila, &tamanho).frequencia);
+    //printf("%c, %c, %c, %d, %d, %d", fila[0].caracter, fila[1].caracter, fila[2].caracter, fila[0].frequencia, fila[1].frequencia, fila[2].frequencia);
 
 
 
+    return 0;
 }
 
 
